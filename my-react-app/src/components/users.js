d7ef3af9a17553eb43ba4fdad2e6ff82fe69df75
Abdul Raheem <abdul.raheem@gove.co>
2024-04-30 20:15:07 +0530

    First Commit

---
+++ my-react-app/src/components/users.js
@@ -0,0 +1,25 @@
+const users = [
+    {
+        id:"1",
+        Name:"Abdul",
+        Email:"abdulraheem.t2002@gmail.com",
+        PhoneNumber:"9384919831",
+        Age:22
+    },
+    {
+        id:"2",
+        Name:"Raheem",
+        Email:"raheem.t2002@gmail.com",
+        PhoneNumber:"9999999999",
+        Age:23
+    },
+    {
+        id:"3",
+        Name:"Abdhullah",
+        Email:"abdhullah2001@gmail.com",
+        PhoneNumber:"96969696969",
+        Age:24
+    }
+];
+
+export default users;
